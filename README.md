e4e3015ab3e8e4cbc182dfc6ceb59d4c49be759d
YanaSergeevna <bondarenko.yana.sergeevna@gmail.com>
2020-05-30 22:41:23 +0300

    Add bootstrap

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>My first website in pure HTML and CSS</h1>
+<h2><a href="https://yanasergeevna.github.io/Responsive-Bootstrap-Site/">Мастер. Все для салонов Красоты</a></h2>
+
---
+++ js/js.js
@@ -0,0 +1,56 @@
+
+		$('.hits').owlCarousel({
+		    loop:true,
+		    margin:10,
+		    nav:true,
+		    navText: ["", ""],
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        600:{
+		            items:2
+		        },
+		        1000:{
+		            items:4
+		        }
+		    }
+		})
+		$('.caruselReviews').owlCarousel({
+		    loop:true,
+		    margin:10,
+		    nav:true,
+		    navText: ["<img src='images/arrow.png'>", "<img src='images/arrow.png'>"],
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        600:{
+		            items:2
+		        },
+		        1000:{
+		            items:3
+		        }
+		    }
+		})
+		$('.Companylist').owlCarousel({
+		    loop:true,
+		    margin:10,
+		    nav:true,
+		    navText: ["<img src='images/arrow.png'>", "<img src='images/arrow.png'>"],
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        1000:{
+		            items:1
+		        }
+		    }
+		})
+
+		$('.glyphicon-cloud').click(function(){
+				$('nav').fadeOut(3000);
+				$('nav').fadeIn(3000, function(){
+					alert('qwerty');
+				});
+		});
